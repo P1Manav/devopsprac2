aace839ec1998ecb599c7c86d6c172be260ae108
P1Manav <22bce281@nirmauni.ac.in>
2025-07-23 01:34:19 +0530

    maybe final 2

--- prac2/src/App.tsx
+++ prac2/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:3000/api/users";
+const API = "https://devopsprac2.onrender.com";
 
 export default function App() {
   const [method, setMethod] = useState("GET");
