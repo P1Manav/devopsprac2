cbc47c7f30ad0ec2e90295de6dce2d20016c08d0
P1Manav <22bce281@nirmauni.ac.in>
2025-08-12 22:30:07 +0530

    firebase issue resolved

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // FIREBASE SETUP
-const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64").toString("utf8")
-);
+const serviceAccount = require("./firebase-key.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
